72dc1ca0d1d7da37f2f91a720c33bac3d175c799
bot hacker <68746883+hackerjh@users.noreply.github.com>
2023-04-20 13:05:56 +0530

    Changes by satya
    The..

--- streamranger.ino
+++ streamranger.ino
@@ -89,8 +89,8 @@ const int PWMLightChannel = 3;
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 
-const char* ssid     = "MyWiFiCar";
-const char* password = "12345678";
+const char* ssid     = "GPB_STREAMRANGER";
+const char* password = "GPbhadrak";
 
 AsyncWebServer server(80);
 AsyncWebSocket wsCamera("/Camera");
